c71dd16c379beb57a704da1d7f0e4005fc7ec6dd
Vera Peuntinger <vera@peuntinger.de>
2016-06-06 16:52:01 +0200

    bei der Regestrierung wird jetzt nur noch true oder false zurückgegeben, kein httpStatuscode mehr

--- src/main/java/de/htw/chatclient/Main.groovy
+++ src/main/java/de/htw/chatclient/Main.groovy
@@ -1,9 +1,6 @@
 package de.htw.chatclient
 
 import de.htw.chatclient.service.RegisterService
-import groovyx.net.http.HttpResponseException
-
-import javax.ws.rs.core.Response
 
 /**
  * Created by vera on 31.05.16.
@@ -14,20 +11,12 @@ class Main {
         println('Application started')
 
 // Beispielhafter Aufruf für Regestrierung eines Users
-// 200 OK --> User wurde regestriert
-// 409 CONFLICT --> mit der Mail gibt es bereits einen User
+// true --> User wurde regestriert
+// false --> mit der Mail gibt es bereits einen User
         RegisterService registerController = new RegisterService();
-        try {
+        boolean register = registerController.register('Anna', 'mail422@mail.de', '123')
 
-            def register = registerController.register('Anna', 'mail2@mail.de', '123') // 200 Ok wenn User regestriert
-        } catch (e) {
-            if (e instanceof HttpResponseException) {
-                e.statusCode == Response.Status.CONFLICT.statusCode // Conflict wenn email bereits regestriert
-                println("status code: " + e.statusCode)
-            }
-            println("error: " + e)
-        }
-
+        println(register)
     }
 
 }
