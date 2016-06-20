d2a3d782536a6138c0e03f2e37887d875cabeee6
Sarah Ganter <sa.ganter@web.de>
2016-06-21 01:18:20 +0200

    render-Aufruf in Event Handler korrigiert

--- src/main/java/de/htw/chatclient/views/LoginView.groovy
+++ src/main/java/de/htw/chatclient/views/LoginView.groovy
@@ -27,7 +27,7 @@ class LoginView {
 
 
             stage(title: "ConnectMe Login", visible: true) {
-                scene(width: 1000, height: 775) {
+                scene(id: "loginScene", width: 1000, height: 775) {
 
                     borderPane() {
 
@@ -80,14 +80,11 @@ class LoginView {
                                             def loginSuccessful = loginService.login(mail, password)
                                             // TODO
                                             if (loginSuccessful) {
-
-                                                RegisterView registerview = new RegisterView()
-                                                registerview.open()
                                                 // wenn erfolgreich --> view wechseln
+                                                new RegisterView().show()
 
                                             } else {
                                                 // wenn nicht erfolgreich --> Fehler anzeigen (Popup)
-                                                new RegisterView().show()
                                             }
                                         }
                                     }
@@ -99,6 +96,99 @@ class LoginView {
 
                     }
                 }
+                //*************** scene (RegisterView) Start ***************
+                scene(id: "registerScene", width: 1000, height: 775) {
+
+                    borderPane() {
+
+                        top() {
+                            hbox(padding: 25, style: "-fx-background-color: #C8F6FF") {
+                                hyperlink(text: "Einloggen")
+                                hyperlink(text: "Registrieren")
+                            }
+                        }
+                        right(align: "center") {
+                            hbox(padding: 25, style: "-fx-background-color: white") {}
+                        }
+                        left(align: "center") {
+                            hbox(padding: 25, style: "-fx-background-color: white") {}
+                        }
+                        bottom(align: "center") {
+                            hbox(padding: 25, style: "-fx-background-color: white") {}
+                        }
+
+                        center() {
+                            hbox(style: "-fx-background-color: white", alignment: "CENTER") {
+                                gridPane(hgap: 20, vgap: 12, padding: 25, alignment: "CENTER") {
+
+                                    label("Werde Teil von ConnectMe", style: "-fx-font-size: 24px;", row: 0, columnSpan: 2, halignment: "center",
+                                            margin: [0, 0, 10]) {}
+
+                                    label("Benutzername", row: 1, column: 0)
+                                    textField(id: "usernameTextField", promptText: "z.B. anna123", row: 1, column: 1 )
+
+                                    label("E-Mail", row: 2, column: 0)
+                                    textField(id: "emailTextField", promptText: "z.B. user@123.de", row: 2, column: 1)
+
+                                    label("Passwort", row: 3, column: 0)
+                                    passwordField(id: "passwordField", promptText: "Passwort eingeben", row: 3, column: 1)
+
+                                    label("Passwort wiederholen", row: 4, column: 0)
+                                    passwordField(promptText: "Passwort wiederholen", row: 4, column: 1)
+
+                                    button(id: "registrationButton", "Registrier dich jetzt", row: 5, column: 0, columnSpan: 2, style: "-fx-base: #29CCE9;") {
+                                        onMouseClicked {e ->
+                                            RegisterService registerService = new RegisterService();
+                                            String name = usernameTextField.getText()
+                                            String mail = emailTextField.getText()
+                                            String password = passwordField.getText()
+                                            // TODO prüfen, ob Passwort = Wiederholung und ob alle Felder gesetzt
+
+                                            def registerSuccessfull = registerService.register(name, mail, password)
+
+                                            if (registerSuccessfull) {
+                                                // TODO bestätigen
+                                                new LoginView().show()
+                                            } else {
+                                                // TODO Fehlermeldung für Benutzer
+
+                                            }
+
+                                        }
+                                    }
+                                    registrationButton.setMinWidth(310)
+
+//                                    //******** EventHandler Reg_Button START *********
+//                                    registrationButton.setOnAction(new EventHandler<ActionEvent>() {
+//                                        //@Override
+//                                        public void handle(ActionEvent event) {
+//                                            String name = GridPane.this.usernameTextField.getText()
+//                                            String email = emailTextField.getText()
+//                                            String passwort = passwordField.getText()
+//
+//                                            RegisterService regService = new RegisterService()
+//                                            //TODO prüfe ob Mail, Name, PW vorhanden
+//                                            boolean isSuccessful = (Boolean)regService.register(name, email, passwort)
+//                                            if (isSuccessful){
+//                                                registrationButton.setText("success")
+//                                            }
+//                                            else {
+//                                                registrationButton.setText("fail")
+//                                            }
+//                                        }
+//                                    })
+//                                    //******** EventHandler Reg_Button ENDE *********
+
+
+                                }
+
+                            }
+                        }
+
+                    }
+                }
+                //*************** scene (RegisterView) Ende ***************
+
             }
         }
     }
